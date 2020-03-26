b9c66e1fafbb006fb0aeee3d5844258f28ebf171
Max Denner <maxdenner027@hotmail.com>
2020-03-26 09:17:57 -0300

    Commit da nova estrutura do projeto
    Signed-off-by: Max Denner <maxdenner027@hotmail.com>

---
+++ projetoToDo/src/main/java/br/com/max/projetos/todolist/bd/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package br.com.max.projetos.todolist.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/projeto","root","");
+		} catch (ClassNotFoundException e ) {
+			throw new RuntimeException(e);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
+	}
+}
