62ddaf1023d119483084e1a0e5087b1782acf6ab
Max Denner <maxdenner027@hotmail.com>
2020-03-26 08:35:02 -0300

    Initial Commit
    Signed-off-by: Max Denner <maxdenner027@hotmail.com>

---
+++ toDoList/src/br/com/max/projetos/todolist/bd/ConnectionFactory.java
@@ -0,0 +1,16 @@
+package br.com.max.projetos.todolist.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/projeto","root","");
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
